ba71ffaf3615aab5c7479c28011cdf23255e4318
Jean Snyman <jean_snyman@hotmail.com>
2019-02-27 16:07:45 +0200

    added body-parser and post request

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.use((req, res, next) => {
     console.log(`Middleware running`)
@@ -10,12 +15,12 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
     console.log(`Route request`)
-    res.send('<h1>Hello World!</h1>')
+    res.send('<form action="/api/products" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-app.get('/api/products', (req, res) => {
-    console.log(`Products`)
-    res.send('<h1>Products!</h1>')
+app.post('/api/products', (req, res) => {
+    console.log(req.body);
+    res.send(req.body)
 })
 
 
