feb3de5097ee702b7427b92dc15166feb101390a
Jean Snyman <jean_snyman@hotmail.com>
2019-02-27 15:55:35 +0200

    added middleware and routes

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
-console.log('Node code')
-console.log('Node code 2')
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+app.use((req, res, next) => {
+    console.log(`Middleware running`)
+    next()
+})
+
+app.get('/', (req, res) => {
+    console.log(`Route request`)
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/products', (req, res) => {
+    console.log(`Products`)
+    res.send('<h1>Products!</h1>')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
