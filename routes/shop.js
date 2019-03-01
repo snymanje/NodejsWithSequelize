dada70a4b65fe7a9886b39b1110f823d595d6705
Jean Snyman <jean_snyman@hotmail.com>
2019-03-01 11:23:12 +0200

    added ejs

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,22 @@
 const path = require('path');
-const rootDir = require('../utils/path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-})
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+});
 
 module.exports = router;
